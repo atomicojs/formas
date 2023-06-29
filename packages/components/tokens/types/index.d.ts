c84de51a04ea2362608843036f18e1196d2cfe46
uppercod <uppercod.git@gmail.com>
2023-06-28 23:50:31 -0400

    save

---
+++ packages/components/tokens/types/index.d.ts
@@ -0,0 +1,9 @@
+export declare const GenericTokens: import("atomico").Sheet;
+export declare const GenericStateTokens: import("atomico").Sheet;
+export declare const NavigationTokens: import("atomico").Sheet;
+export declare const ButtonTokens: import("atomico").Sheet;
+export declare const DropdownTokens: import("atomico").Sheet;
+export declare const CheckboxTokens: import("atomico").Sheet;
+export declare const RadioTokens: import("atomico").Sheet;
+export declare const SwitchTokens: import("atomico").Sheet;
+export declare const InputTokens: import("atomico").Sheet;
